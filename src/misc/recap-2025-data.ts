6318bcd8d6c35aac561132cb092c03cdb60286fd
HYUNFLIX <hyunnet@gmail.com>
2025-12-24 22:10:56 +0900

    update

---
+++ src/misc/recap-2025-data.ts
@@ -0,0 +1,12 @@
+export const data = {
+  date: '2025년 12월 21일',
+  people: 1860938,
+  startCount: 9239646,
+  busiestTimeFrom: 12,
+  busiestTimeTo: 13,
+  busiestDateMonth: 11,
+  busiestDateDate: 11,
+  marbleCount: 1255589798,
+  country: 147,
+  coffeeBuyer: 34,
+} as const;
